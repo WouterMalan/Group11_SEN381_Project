570d3886773b835b442514852690a01d560c71fe
EuthonW <walterseuthon@gmail.com>
2022-10-12 18:21:21 +0200

    Nice

---
+++ Group11_SEN381_Project/DataAccess/DataHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group11_SEN381_Project.DataAccess
+{
+    class DataHandler
+    {
+        string connection = "Server=Nemaiz; Initial Catalog=VaccineReg; "
+    }
+}
