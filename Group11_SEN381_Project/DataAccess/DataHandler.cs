1fb7c7082d7ec87d35118391b071dd48211bcbd1
EuthonW <walterseuthon@gmail.com>
2022-10-12 18:31:53 +0200

    Basic

--- Group11_SEN381_Project/DataAccess/DataHandler.cs
+++ Group11_SEN381_Project/DataAccess/DataHandler.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Group11_SEN381_Project.DataAccess
 {
     class DataHandler
     {
-        string connection = "Server=Nemaiz; Initial Catalog=VaccineReg; "
+        string connection = "Server=Nemaiz; Initial Catalog=MedicalCallCentre; Integrated Security=SSPI";
+        public DataTable getPolicy()
+        {
+            SqlConnection cn = new SqlConnection(connection);
+            SqlDataAdapter ad = new SqlDataAdapter("getPolicy", cn);
+            ad.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            return dt;
+        }
     }
 }
