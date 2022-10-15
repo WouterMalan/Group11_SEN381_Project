bda3eb0485d44c4a0374ae67fe4e8ff35ca48955
Wouter <woutermalan7909@gmail.com>
2022-10-15 11:08:13 +0200

    Added private variables and constructors for Client class

--- Group11_SEN381_Project/BusinessLogic/Client.cs
+++ Group11_SEN381_Project/BusinessLogic/Client.cs
@@ -9,14 +9,27 @@ namespace Group11_SEN381_Project.BusinessLogic
     class Client
     {
         private string FullName;
+        private string Address;
         private int PhoneNum;
+        private string Email;
         private string Dependants;
         private int NatID;
         public Client()
         {
 
         }
-        public void createClient(string FullName, int PhoneNum, string Dependants, int NatID) 
+
+        public Client(string fullName, string address, int phoneNum, string email, string dependants, int natID)
+        {
+            FullName = fullName;
+            Address = address;
+            PhoneNum = phoneNum;
+            Email = email;
+            Dependants = dependants;
+            NatID = natID;
+        }
+
+        public void createClient(string FullName,string Address, int PhoneNum,string Email, string Dependants, int NatID) 
         {
         
         }
